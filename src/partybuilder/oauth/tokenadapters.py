ed02b2ead1b6f9aeffe2ee2a213213a9f8735954
paul <prsephton@gmail.com>
2017-12-12 14:48:04 +0200

    A better google adapter

--- src/partybuilder/oauth/tokenadapters.py
+++ src/partybuilder/oauth/tokenadapters.py
@@ -7,7 +7,7 @@
 '''
 import grok
 from interfaces import ITokenRequest, IOAuthPrincipal, IOAuthPrincipalSource
-from six.moves.urllib.request import urlopen
+from six.moves.urllib.request import urlopen, Request
 from six.moves.urllib.parse import urlencode
 
 
@@ -23,33 +23,30 @@ class GoogleTokenToUser(grok.Adapter):
     def __new__(self, token):
         app = grok.getApplication()
         users = IOAuthPrincipalSource(app)
-        uid = u"Google.{}".format(users.sequence)
-
-        found = users.find(id=uid)
-        if len(found)==0:
-            user = users.new(id=uid)
-        else:
-            user = found[0]
-
-        user.authInfo = token.info
 
         url = u"https://www.googleapis.com/userinfo/v2/me"
         req = Request(url)
+        req.add_header("Content-Type", "application/json")
         req.add_header("Authorization", "{} {}".format(token.info['token_type'],
                                                        token.info['access_token']))
         res = urlopen(req).read()
         if res: res = json.loads(res)
         if res is None:
-            user.title = u''
-            user.description = u''
+            return None
         else:
+            uid = u"Google.{}".format(res['id'])
+            found = users.find(id=uid)
+            if len(found)==0:
+                user = users.new(id=uid)
+            else:
+                user = found[0]
+            user.authInfo = token.info
             user.title = res['name']
-            res.description = u'{} {}'.format(res['given_name'], res['family_name'])
-
-        user.domain = u'Google'
-        user.login = token.info['id_token']
-        user.secret = token.info['access_token']
-        return user
+            user.description = u'{} {}'.format(res['given_name'], res['family_name'])
+            user.domain = u'Google'
+            user.login = token.info['id_token']
+            user.secret = token.info['access_token']
+            return user
 
 
 class FacebookTokenToUser(grok.Adapter):
