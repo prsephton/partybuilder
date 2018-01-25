7299ff2fc0d8393fa9715ebe8a39e88f50352d9b
paul <prsephton@gmail.com>
2018-01-25 19:26:16 +0200

    Click handler for skill choices

--- src/partybuilder/static/utils.js
+++ src/partybuilder/static/utils.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
 	
+	$("button.skill_choice").on('click', function(){
+		$("#skill_choice").val($(this).attr("skill_choice"));
+	});
+	
 	$("input.autosubmit").on("change", function(){
 		var form = $(this).parents('form').first();
 		$(".message", form).css("display", "block");
